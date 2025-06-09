42175bc94d23b37576368cdea4463875cee61eaa
Odusola Daniel <162451976+SODUSOLA@users.noreply.github.com>
2025-06-09 18:54:53 +0100

    Fix import/export bugs and resolve server startup issues for group routes and middleware

--- Routes/groupRoute.js
+++ Routes/groupRoute.js
@@ -1,11 +1,13 @@
 import express from 'express';
 // import {createGroup, getGroups, getGroupById, updateGroup, deleteGroup} from '../Controllers/groupController.js';
 import { createGroup } from '../Controllers/groupController.js';
-import { protect } from '../Middleware/authMiddleware.js';
+import { protect } from '../Middlewares/authMiddleware.js';
+
 
 const router = express.Router();
 
 // Route to create a new group
 router.post('/', protect,createGroup);
+
 
 export default router;
