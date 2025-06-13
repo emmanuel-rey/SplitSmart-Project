afcb56ebab18688953241f8b0b039ef210a5bf9c
Odusola Daniel <162451976+SODUSOLA@users.noreply.github.com>
2025-06-13 02:51:14 +0100

    expenseRoute.js

--- Routes/expenseRoute.js
+++ Routes/expenseRoute.js
@@ -2,7 +2,7 @@ import express from 'express';
 import {
     addExpense,
     getExpensesByGroup
-} from '../controllers/expenseController.js';
+} from '../Controllers/expenseController.js';
 
 const router = express.Router();
 
